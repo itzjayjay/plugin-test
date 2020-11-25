b056c1605a2babcde8548f18c51359dfebf76a81
itzjayjay <61878409+itzjayjay@users.noreply.github.com>
2020-11-24 23:08:17 -0800

    Update TestFile.php

--- TestPlugin/src/Test/TestFile.php
+++ TestPlugin/src/Test/TestFile.php
@@ -1,20 +1,36 @@
 <?php
 
-namespace Inactive-to-Reactive;
+namespace Test;
 
 use pocketmine\plugin\PluginBase;
 
-class Inactive-to-ReactiveExample extends PluginBase{
+use pocketmine\event\player\PlayerJoinEvent;
+
+use pocketmine\event\player\PlayerQuitEvent;
+
+use pocketmine\Player;
+
+use pocketmine\Server;
+
+use pocketmine\event\Listener;
+
+use pocketmine\utils\TextFormat as C;
+
+class Test extends PluginBase implements Listener{
 
           public function onLoad(){
                     $this->getLogger()->info("Plugin Loading");
           }
           public function onEnable(){
-                    $this->getLogger()->info("Enabled Plugin");
+                    $this->getServer()->getPluginManager()->registerEvents($this,$this);
+		    $this->getLogger()->info("Enabled Plugin");
           }
           public function onDisable(){
                     $this->getLogger()->info("Plugin Disabled");
           }
+	  public function onJoin(PlayerJoinEvent $event){
+  		 $player = $event->getPlayer();
+   		$name = $player->getName();
+   		$this->getServer()->broadcastMessage(C::GREEN."$name Joined The Test Server! Awesome!");
+	}
 }
-
-?>
